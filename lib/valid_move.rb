d9abe51c7d8e074da785764a77a26ebb0739c607
Charles Lai <charlesangeloplai@gmail.com>
2020-08-22 22:50:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index >= 0 || index <= 8
-    true
+  f position_taken(board, index) == true
+    return nil
+
   end
 end
 
