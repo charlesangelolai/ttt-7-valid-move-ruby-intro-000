4d8a54857bbc5ea21056beccfc2a3fae36f86a82
Charles Lai <charlesangeloplai@gmail.com>
2020-08-22 22:46:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if index >= 0 && index <= 8
     true
-  elsif position_taken?(board, index) == false
+  elsif position_taken?(board, index) == true
     false
   else
     false
