52573569655183a5756df8a79a3a9c26f9b5ed66
Charles Lai <charlesangeloplai@gmail.com>
2020-08-22 22:43:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,10 @@
 def valid_move?(board, index)
   if index >= 0 && index <= 8
     true
+  else
+    false
   end
+
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
